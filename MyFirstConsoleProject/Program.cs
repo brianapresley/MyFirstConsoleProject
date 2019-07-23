146925f2e6b900ee2d223e39aefa0d0d1c816246
Briana Presley <presley.briana@gmail.com>
2019-07-23 16:32:29 -0400

    Made a change in program.cs

---
+++ MyFirstConsoleProject/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MyFirstConsoleProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int answer = Arithmetic.Add(3, 7);
+            Console.WriteLine("3 + 7 = " + answer);
+            answer = Arithmetic.Sub(10, 6);
+            Console.WriteLine("10 - 6 = " + answer);
+            answer = Arithmetic.mult(5, 7);
+            Console.WriteLine("5 * 7 = " + answer);
+            answer = Arithmetic.div(7, 2);
+            Console.WriteLine("7 / 2 = " + answer);
+        }
+    }
+    class Arithmetic {
+        public static int Add (int a, int b) {
+            return a + b;
+           }
+        public static int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public static int mult(int a, int b)
+        {
+            return a * b;
+        }
+        public static int div(int a, int b)
+        {
+            return a / b;
+        }
+       }
+}
